27a054f9ef66855905a750d4531ebcddf5ac7199
zaLTys <povilas.simanskas@gmail.com>
2022-01-22 06:54:19 +0200

    Adding Roles

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -55,7 +55,8 @@ namespace JwtWebApi.Controllers
         {
             List<Claim> claims = new List<Claim>()
             {
-                new Claim(ClaimTypes.Name, user.Username)
+                new Claim(ClaimTypes.Name, user.Username),
+                new Claim(ClaimTypes.Role, "Admin")
             };
 
             var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(
